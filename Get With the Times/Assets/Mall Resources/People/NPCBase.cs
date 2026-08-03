0748c5626c935974ca4ee6e4bfd1cddf4a31578e
Supcon <136843005+Supconman@users.noreply.github.com>
2026-08-02 22:27:21 -0400

    They Move Now
    They Move Now

---
+++ Get With the Times/Assets/Mall Resources/People/NPCBase.cs
@@ -0,0 +1,115 @@
+using System.Security.Cryptography;
+using Unity.IO.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.AdaptivePerformance.Provider;
+
+public class NPCBase : MonoBehaviour {
+    //for roaming behavior
+    public bool roams = true;
+    public Vector3 homeLocation = Vector3.zero;
+    public float stopMax = 5.0f;
+    public float moveMax = 10.0f;
+    public float roamSpeed = 1.0f;
+    public float roamMaxDist = 10.0f;
+    [HideInInspector] public Vector3 destination = Vector3.zero;
+    [HideInInspector] public float roamTimer = 0;
+    [HideInInspector] public bool walking = false;
+
+
+    //for making the child a billboard
+    public bool hasSprite = true;
+    GameObject mainCamera;
+    [HideInInspector] public GameObject childSprite;
+
+    public virtual void NPCInit() {
+
+        if (roams) {
+            if (homeLocation.Equals(Vector3.zero)) {
+                homeLocation = transform.position;
+            }
+            destination = homeLocation;
+        }
+
+
+        if (hasSprite) {
+            mainCamera = GameObject.Find("PlayerCamera");
+            childSprite = transform.GetChild(0).gameObject;
+            childSprite.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    //for handling interactions 
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Interact") {
+            interactBehavior(other);
+        }
+    }
+    //as above
+    public virtual void interactBehavior(Collider other) {
+
+    }
+
+    //for making the sprite face the camera
+    public virtual void spriteBehavior() {
+        if (hasSprite) {
+            childSprite.transform.LookAt(new Vector3(mainCamera.transform.position.x, childSprite.transform.position.y, mainCamera.transform.position.z));
+        }
+    }
+
+    //for making the NPC's 'Randomly' walk around the mall
+    public virtual void roamTick() {
+        //changing states
+        if (!walking && roamTimer <= 0) {
+            roamPickTarget();
+        }
+        else if (walking && roamTimer <= 0) {
+            roamStop();
+        }
+        else if (roamTimer <= 0) {
+            Debug.Log("Check The Roam Code, walking bool isn't set properly");
+            roamTimer = 10.0f;
+        }
+        //performing state actions
+        else if (!walking) {
+            roamIdle();
+        }
+        else if (walking) {
+            roamWalk();
+        }
+        else {
+            Debug.Log("How tf did you activate this?");
+        }
+    }
+
+    public virtual void roamPickTarget() {
+        destination = new Vector3(homeLocation.x + Random.Range(-roamMaxDist, roamMaxDist), 0, homeLocation.z + Random.Range(-roamMaxDist, roamMaxDist));
+
+        roamTimer = Random.Range(0, moveMax);
+        walking = true;
+
+        if (hasSprite) {
+            childSprite.GetComponent<Animator>().enabled = true; 
+        }
+    }
+
+    public virtual void roamStop() {
+        roamTimer = Random.Range(0, stopMax);
+        walking = false;
+
+        if (hasSprite) {
+            childSprite.GetComponent<Animator>().enabled = false;
+        }
+    }
+
+    public virtual void roamIdle() {
+        roamTimer -= Time.deltaTime;
+    }
+
+    public virtual void roamWalk() {
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(destination.x - transform.position.x, 0, destination.z - transform.position.z) * roamSpeed);
+        roamTimer -= Time.deltaTime;
+    }
+    
+
+
+}
--- Get With the Times/Assets/Mall Resources/People/NonSubjectPerson.cs
+++ Get With the Times/Assets/Mall Resources/People/NonSubjectPerson.cs
@@ -1,24 +1,36 @@
 using UnityEditor.UI;
 using UnityEngine;
 
-public class NonSubjectPerson : MonoBehaviour {
+public class NonSubjectPerson : NPCBase {
+
+    public float speakPitch = 100.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
+        NPCInit(); 
 
+        if(speakPitch == 100.0f) {
+            speakPitch = Random.Range(0.5f, 1.5f);
+        }
+        gameObject.GetComponent<AudioSource>().pitch = speakPitch;
     }
 
     // Update is called once per frame
     void Update() {
-
+        roamTick(); 
     }
 
     //interaction behavior
-    private void OnTriggerEnter(Collider other) {
-        if (other.tag == "Interact") {
-            gameObject.GetComponent<AudioSource>().Play();
-            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-5.0f,5.0f), Random.Range(-2.0f, 3.0f), Random.Range(-5.0f, 5.0f)), ForceMode.Impulse);
-            Debug.Log("Interaction");
+    public override void interactBehavior(Collider other) {
+        gameObject.GetComponent<AudioSource>().Play();
+        gameObject.GetComponent<Rigidbody>().AddForce((new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-2.0f, 3.0f), Random.Range(-5.0f, 5.0f))), ForceMode.Impulse);
+        if (base.hasSprite) {
+            base.childSprite.GetComponent<Animator>().enabled = true; 
         }
-        
+    }
+
+    //make the sprite face towards the camera
+    private void LateUpdate() {
+        spriteBehavior();
     }
 }
