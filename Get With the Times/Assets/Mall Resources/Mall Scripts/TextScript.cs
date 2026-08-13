5bfd04e8c31795f654a5e0ad91cd19f46881b5e9
Supcon <136843005+Supconman@users.noreply.github.com>
2026-08-13 00:26:11 -0400

    Add Majora's Mask Thingy

---
+++ Get With the Times/Assets/Mall Resources/Mall Scripts/TextScript.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class TextScript : MonoBehaviour
+{
+    TMP_Text text;
+    AudioSource sound; 
+    
+    int day = 0;
+
+    public float appearTime = 2.0f;
+    public float disappearTime = 5.0f;
+    public float disappearSpeed = 1.0f;
+    float curTime = 0;
+    bool appearedYet = false;
+
+    public List<string> DayText = new List<string>() {"?? Days Remain", "5 Days Remain", "4 Days Remain", "3 Days Remain", "2 Days...", "The Final Day" };
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+        day = GlobalManager.globalInstance.day;
+        sound = GetComponent<AudioSource>();
+
+        if (day <= 5) {
+            text.text = DayText[day];
+        }
+        else {
+            text.text = "Someone Broke the Day Counter";
+        }
+
+        text.color = new Vector4(text.color.r, text.color.g, text.color.b, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curTime += Time.deltaTime;
+
+        if(!appearedYet && curTime >= appearTime) {
+            text.color = new Vector4(text.color.r, text.color.g, text.color.b, 1.0f);
+            sound.Play();
+            appearedYet = true;
+        }
+        else if (appearedYet && curTime >= disappearTime) {
+            if(text.color.a == 0.0f) {
+                Destroy(gameObject);
+            }
+            text.color = new Vector4(text.color.r, text.color.g, text.color.b, text.color.a - disappearSpeed * Time.deltaTime);
+        }
+        
+    }
+}
--- Get With the Times/Assets/Mall Resources/People/Rizz/RizzPerson.cs
+++ Get With the Times/Assets/Mall Resources/People/Rizz/RizzPerson.cs
@@ -5,8 +5,6 @@ using UnityEngine.SceneManagement;
 public class RizzPerson : NPCBase {
 
     public float speakPitch = 100.0f;
-    bool changingScene = false;
-    bool changingSceneWaitOne = true; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -26,26 +24,12 @@ public class RizzPerson : NPCBase {
     //interaction behavior
     public override void interactBehavior(Collider other) {
         gameObject.GetComponent<AudioSource>().Play();
-        childSprite.GetComponent<SpriteRenderer>().enabled = false; 
-        changingScene = true;
-        changingSceneWaitOne = true;
+        childSprite.GetComponent<SpriteRenderer>().enabled = false;
+        SceneManager.LoadScene("BattleScene");
     }
 
     //make the sprite face towards the camera
     private void LateUpdate() {
         spriteBehavior();
-        if (changingScene && !changingSceneWaitOne) {
-            //I'm not cleaning this texture up, since we'll only have like 5 of them, but be aware of that in case this use case expands.  
-            GlobalManager.globalInstance.battleBackground = ScreenCapture.CaptureScreenshotAsTexture();
-            GlobalManager.globalInstance.battleBackground.Apply();
-
-            SceneManager.LoadScene("BattleScene");
-
-            changingScene = false; 
-        }
-        else if (changingScene) {
-            childSprite.GetComponent<SpriteRenderer>().enabled = false;
-            changingSceneWaitOne = false; 
-        }
     }
 }
